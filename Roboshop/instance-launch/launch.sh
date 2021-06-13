8dd87c722adf67741ce653dae65087fd1b77a677
korek <koreking12@gmail.com>
2021-06-13 18:49:09 +0530

    Things are updated

--- Roboshop/instance-launch/launch.sh
+++ Roboshop/instance-launch/launch.sh
@@ -14,7 +14,8 @@ LVER=1
 ## Validate If Instance is already there
 
 INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Value=components" | jq .Reservations[].Instances[].state.Name | xargs -n1)
-if  [ "${INSTANCE_STATE}" = "running"]; then
+if  [ "${INSTANCE_STATE}" = "running" ]; then
   echo "instance already exist!!"
   exit 0
 fi
+
