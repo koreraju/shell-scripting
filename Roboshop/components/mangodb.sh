1acc8f415bca7ffe3226043634c8cdd425b8329d
korek <koreking12@gmail.com>
2021-06-13 15:26:49 +0530

    Things are updated

---
+++ Roboshop/components/mangodb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source components/common.sh
+rm -f /tmp/Roboshop.log
+
+HEAD "setup MongoDB yum repo file\t"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT $?
+
+HEAD "install MongoDB\t\t"
+yum install -y mongodb-org &>>/tmp/Roboshop.log
+STAT $?
+
+HEAD "start mongoDB service"
+systemctl enable mongod &>>/tmp/Roboshop.log
+systemctl start mongod &>>/tmp/Roboshop.log
+STAT $?
