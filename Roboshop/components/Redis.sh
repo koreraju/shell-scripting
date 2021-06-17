b806fd8108ccd6115a18d890e647235ab5c11fca
korek <koreking12@gmail.com>
2021-06-17 22:19:24 +0530

    Things are updated

--- Roboshop/components/Redis.sh
+++ Roboshop/components/Redis.sh
@@ -5,11 +5,11 @@ rm -f /tmp/Roboshop.log
 set-hostname Redis
 
 HEAD "setup Redis Repos"
-
 yum install epel-release yum-utils -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/Roboshop.log && yum-config-manager --enable remi &>>/tmp/Roboshop.log
 STAT $?
 
 HEAD "install redis"
 yum install Redis -y &>>/tmp/Roboshop.log
 STAT $?
+
 
